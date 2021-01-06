7a14b33f4162f011c53ebeeecab5d06a441ec750
teacher <teacher@aahn02.edu>
2021-01-06 10:58:30 +0700

    fix index.php on heroku

---
+++ index.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>PHP tutorial</title>
+	<meta charset="utf-8">
+</head>
+<body>
+	<h1 style="text-align: center;">Welcome to learn PHP</h1>
+</body>
+</html>
